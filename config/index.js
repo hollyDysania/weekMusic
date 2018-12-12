cc3178e3d3fac8d9f93b31740d7b2f643041f79f
hollyDysania <holly_dysania@sina.com>
2018-12-12 14:21:00 +0800

    修改host0.0.0.0

--- config/index.js
+++ config/index.js
@@ -35,7 +35,7 @@ module.exports = {
     },
     // Various Dev Server settings
     // host: 'localhost', 
-    host: '192.168.2.110', 
+    host: '0.0.0.0', 
     // can be overwritten by process.env.HOST
     port: 8000, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
