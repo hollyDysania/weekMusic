acca82a70106666f90b4097b848740a9fa857ce8
holly <holly_dysania@sina.com>
2019-04-26 11:18:49 +0800

    chore:多项目部署更改打assetsPublicPath与路由base

--- config/index.js
+++ config/index.js
@@ -79,7 +79,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/music/',
     /**
      * Source Maps
      */
--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,7 @@ Vue.use(Router)
 
 export default new Router({
   mode: 'history',
+  base: '/music/',
   routes: [
     {
       path: '/',
